d36076cbc87202ebf55341045927aa8bf4fefc95
Ke <kezhang@kth.se>
2022-04-19 00:27:28 +0200

    combined data e2q3

---
+++ Assignment-II/e2/q3/combine.sh
@@ -0,0 +1,14 @@
+rm stream_guided_out.txt
+#for n in {16,32,64,128,256,512,1024,2048}
+#do 
+#	$cat stream_guided$n_\output.txt>>stream_guided_out.txt
+#done
+cat stream_guided16_output.txt>>stream_guided_out.txt
+cat stream_guided32_output.txt>>stream_guided_out.txt
+cat stream_guided64_output.txt>>stream_guided_out.txt
+cat stream_guided128_output.txt>>stream_guided_out.txt
+cat stream_guided256_output.txt>>stream_guided_out.txt
+cat stream_guided512_output.txt>>stream_guided_out.txt
+cat stream_guided1024_output.txt>>stream_guided_out.txt
+cat stream_guided2048_output.txt>>stream_guided_out.txt
+
